2a318140ec6ec26db7853f040ce4286055f2163d
PanosTsak12 <84141160+panos140@users.noreply.github.com>
2026-09-16 14:28:26 +0300

    Update application title in love.conf

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = "y2k_music_player"
-    t.title = "Y2K MUSIC PLAYER 3000"
+    t.title = "Y2k Player"
     t.window.width = 460
     t.window.height = 700
     t.window.resizable = false
